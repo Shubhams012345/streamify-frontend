6eca3dbd169ff3500f9712a1f44e5c7d19bd6cff
Shubhams012345 <shubhsingh242004@gmail.com>
2026-01-30 01:20:46 +0530

    updated use login

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,14 +1,28 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login } from "../lib/api";
+import { useNavigate } from "react-router-dom";
 
 const useLogin = () => {
   const queryClient = useQueryClient();
-  const { mutate, isPending, error } = useMutation({
+  const navigate = useNavigate();
+
+  const { mutate: loginMutation, isPending, error } = useMutation({
     mutationFn: login,
-    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+
+    onSuccess: (data) => {
+      // Store auth user in cache
+      queryClient.setQueryData(["authUser"], data);
+
+      // Redirect after successful login
+      navigate("/");
+    },
+
+    onError: (err) => {
+      console.log("Login error:", err);
+    }
   });
 
-  return { error, isPending, loginMutation: mutate };
+  return { error, isPending, loginMutation };
 };
 
 export default useLogin;
